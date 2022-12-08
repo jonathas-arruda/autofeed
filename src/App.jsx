aca3a790b059d09cc9bc0257ee554c19ddbbac16
Jonathas <jonathasarruda@outlook.com>
2022-12-08 10:42:46 -0300

    Application Structure - Component: Sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
-import './global.css'
+
+import styles from "./App.module.css";
+
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
-      <Header/>
-      <Post
-        author="Fulano de Tal"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, perferendis."
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Fulano de Tal"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, perferendis."
+          />
+          <Post
+            author="Fulano de Tal"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, perferendis."
+          />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
+        alt=""
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/75821807?v=4"
+        />
+        <strong>Fulano de Tal</strong>
+        <span> Dev</span>
+      </div>
+      <footer>
+        <a href="">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
